fd6342bfb1cbbcbb009d449ef9acc36e5af6783d
Lamanris <sprite_kg@mail.ru>
2021-01-23 23:32:47 +0400

    first commit

---
+++ js/script.js
@@ -0,0 +1,39 @@
+const selectSingle = document.querySelectorAll('.__select');
+const selectSingle_title = document.querySelectorAll('.__select__title');
+const selectSingle_labels = document.querySelectorAll('.__select__label');
+
+// Toggle menu
+selectSingle_title.forEach((el) =>
+el.addEventListener('click', () => {
+    let closeParent = el.closest('.__select')
+    if ('active' === closeParent.getAttribute('data-state'))
+    {
+        closeParent.setAttribute('data-state', '')
+
+    } else {
+        selectSingle.forEach((el) => el.setAttribute('data-state', ''))
+        closeParent.setAttribute('data-state', 'active')
+    }
+})
+)
+selectSingle_labels.forEach((el) =>
+
+el.addEventListener('click', (evt) => {
+    let closeTitle = el.closest('.__select').firstElementChild
+    let closeParent = el.closest('.__select')
+    closeTitle.textContent = evt.target.textContent
+    closeTitle.classList.add('text-active')
+    closeParent.setAttribute('data-state', '')
+})
+)
+
+const menuTypeItems = document.querySelectorAll('.menu_type-item');
+
+menuTypeItems.forEach((el) => {
+    el.addEventListener('click', () => {
+        let allMenuLists = document.querySelectorAll('.menu_list')
+        allMenuLists.forEach((el) => el.classList.remove('active'))
+        let menuList = document.getElementById(el.getAttribute('data-state'));
+        menuList.classList.add('active')
+    })
+})
---
+++ js/slider.js
@@ -0,0 +1,7 @@
+let specialtySlider = new Swiper('.specialty-swiper-container', {
+    spaceBetween: 400,
+    pagination: {
+        el: '.swiper-pagination',
+        type: 'bullets',
+    },
+});
