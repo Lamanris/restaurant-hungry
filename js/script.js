ad9ec5ab422c85cd0352aba5871e99aad0f3bd67
Lamanris <sprite_kg@mail.ru>
2021-01-28 20:59:49 +0400

    done

--- js/script.js
+++ js/script.js
@@ -1,39 +1,21 @@
 const headerBurger = document.querySelector('.header__burger')
-const headerMenuBurger = document.querySelector('.header__navbar-burger')
-const headerMenu = document.querySelectorAll('.header__navbar')
 const body =  document.querySelector('body')
 const headerMenuItem = document.querySelectorAll('.header__navbar__item')
-const headerLogo = document.querySelector('.header__logo')
 
 headerBurger.addEventListener ('click', () => {
-    headerBurger.classList.toggle('active');
-    headerMenuBurger.classList.toggle('active');
-    headerMenu.forEach((el) => el.classList.toggle('active'));
-    body.classList.toggle('lock')
+    body.classList.toggle('active')
 });
 headerMenuItem.forEach((el) => {
     el.addEventListener('click', () => {
-        headerMenuBurger.classList.remove('active');
-        headerMenu.forEach((el) => el.classList.remove('active'));
-        headerBurger.classList.remove('active');
-        body.classList.remove('lock');
+        body.classList.toggle('active');
     })
 });
-headerLogo.addEventListener('click', () => {
-    headerMenuBurger.classList.remove('active');
-    headerMenu.forEach((el) => el.classList.remove('active'));
-    headerBurger.classList.remove('active');
-    body.classList.remove('lock');
-});
-
-
 
 
 const selectSingle = document.querySelectorAll('.__select');
 const selectSingle_title = document.querySelectorAll('.__select__title');
 const selectSingle_labels = document.querySelectorAll('.__select__label');
 
-// Toggle menu
 selectSingle_title.forEach((el) =>
 el.addEventListener('click', () => {
     let closeParent = el.closest('.__select')
